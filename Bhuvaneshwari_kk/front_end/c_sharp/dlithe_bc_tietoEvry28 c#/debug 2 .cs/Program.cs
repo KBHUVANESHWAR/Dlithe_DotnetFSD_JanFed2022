765f341f886c458424c27b35045e10fb6a8700f7
KBHUVANESHWAR <kbhuvaneshwari28@gmail.com>
2022-02-22 21:38:31 +0530

    task28

---
+++ Bhuvaneshwari_kk/front_end/c_sharp/dlithe_bc_tietoEvry28 c#/debug 2 .cs/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace PropertyDemo
+{
+    class Example
+    {
+        private int _empid, _eage;
+        private string _ename, _eaddress;
+        public int empid
+        {
+            set
+            {
+                _empid = value;
+            }
+            get
+            {
+                return _empid;
+            }
+        }
+        public int eage
+        {
+            set
+            {
+                _eage = value;
+            }
+            get
+            {
+                return _eage;
+            }
+        }
+        public string ename
+        {
+            set
+            {
+                _ename = value;
+            }
+            get
+            {
+                return _ename;
+            }
+        }
+        public string eaddress
+        {
+            set
+            {
+                _eaddress = value;
+            }                                     // get method
+            get
+            {
+                return _eaddress;
+            }
+        }
+        static void Main()                           // Main method
+        {
+            Example example = new Example();
+            example._empid = 107;
+            example._eage = 21;
+            example._ename = "Bhuvaneshwari k";
+            example._eaddress = " mysore";
+            Console.WriteLine("EMP ID: " + example.empid);
+            Console.WriteLine("EMP AGE: " + example.eage);
+            Console.WriteLine("EMP NAME: " + example.ename);
+            Console.WriteLine("EMP ADDRESS: " + example.eaddress);
+        }
+    }
+}
---
+++ Bhuvaneshwari_kk/front_end/c_sharp/dlithe_bc_tietoEvry28 c#/debug1.css/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace debug1
+{
+    class Program
+    {
+        static void Main(string[] args)              // []
+        {
+            int SNO, MARK1, MARK2, MARK3, AVERAGE;               // ;
+            string SNAME;                                        // ;
+            Console.WriteLine("ENTER THE STUDENT DETAILS");
+            Console.WriteLine("ENTER THE STUDENT NO");
+            SNO = int.Parse(Console.ReadLine());                // int.Parse(Console.ReadLine());  
+            Console.WriteLine("ENTER THE STUDENT NAME");
+            SNAME = Console.ReadLine();
+            Console.WriteLine("ENTER THE MARKS OF 3 SUBJECTS");
+            MARK1 = int.Parse(Console.ReadLine());             // Mark 1
+            MARK2 = int.Parse(Console.ReadLine());
+            MARK3 = int.Parse(Console.ReadLine());
+            int TOTAL = MARK1 + MARK2 + MARK3;
+            AVERAGE = TOTAL / 3;                              // /3
+            Console.WriteLine("\n\n\nTHE STUDENT DETAILS ARE GIVEN BELOW :");
+            Console.WriteLine("THE STUDENT NO IS: " + SNO);               // +
+            Console.WriteLine("THE STUUDENT NAME IS: " + SNAME);
+            Console.WriteLine("TOTAL MARKS IS : " + TOTAL);
+            Console.WriteLine("AVEARGE MAARK IS: " + AVERAGE);
+            Console.ReadKey();
+        }
+    }
+}
